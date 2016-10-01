0d60ece9f754b1c83f71db1b11f6fac3afb7ca74
Frederico Pereira <restarac@users.noreply.github.com>
2016-10-01 20:58:36 -0300

    Created by Coding Ground Client

---
+++ New Project-20160930/HelloWorld.scala
@@ -0,0 +1,9 @@
+import java.io._
+
+object HelloWorld {
+   def main(args: Array[String]) {
+        println("calling cessionChooser...")
+        // CessionChooser.main(args)
+        RandomGenerator.main(args)
+   }
+}
